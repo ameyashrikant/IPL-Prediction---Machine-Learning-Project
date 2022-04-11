6e083eea517275b3b91ab821c231226d90953574
ameyashrikant <76768424+ameyashrikant@users.noreply.github.com>
2022-04-11 09:54:20 +0530

    Update First Innings Score Prediction - IPL.py

--- First Innings Score Prediction - IPL.py
+++ First Innings Score Prediction - IPL.py
@@ -1,51 +1,50 @@
-# Importing essential libraries
 import pandas as pd
 import pickle
 
-# Loading the dataset
-df = pd.read_csv('ipl.csv')
+#loadind dataset
+matches = pd.read_csv('ipl.csv')
 
 # --- Data Cleaning ---
 # Removing unwanted columns
 columns_to_remove = ['mid', 'venue', 'batsman', 'bowler', 'striker', 'non-striker']
-df.drop(labels=columns_to_remove, axis=1, inplace=True)
+matches.drop(labels=columns_to_remove, axis=1, inplace=True)
+matches.replace(to_replace ="Delhi Daredevils", value = "Delhi Capitals", inplace = True)
+# Keeping only current teams
+current_teams = ['Chennai Super Kings', 'Delhi Capitals' , 'Kings XI Punjab', 
+                 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals',
+                 'Royal Challengers Bangalore', 'Sunrisers Hyderabad']
 
-# Keeping only consistent teams
-consistent_teams = ['Kolkata Knight Riders', 'Chennai Super Kings', 'Rajasthan Royals',
-                    'Mumbai Indians', 'Kings XI Punjab', 'Royal Challengers Bangalore',
-                    'Delhi Daredevils', 'Sunrisers Hyderabad']
-df = df[(df['bat_team'].isin(consistent_teams)) & (df['bowl_team'].isin(consistent_teams))]
+matches = matches[(matches['bat_team'].isin(current_teams)) & (matches['bowl_team'].isin(current_teams))]
 
-# Removing the first 5 overs data in every match
-df = df[df['overs']>=5.0]
+# Removing the first 6 overs data in every match
+matches = matches[matches['overs']>=6.0]
 
 # Converting the column 'date' from string into datetime object
 from datetime import datetime
-df['date'] = df['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+matches['date'] = matches['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
 
 # --- Data Preprocessing ---
 # Converting categorical features using OneHotEncoding method
-encoded_df = pd.get_dummies(data=df, columns=['bat_team', 'bowl_team'])
+match_df = pd.get_dummies(data=matches, columns=['bat_team', 'bowl_team'])
 
-# Rearranging the columns
-encoded_df = encoded_df[['date', 'bat_team_Chennai Super Kings', 'bat_team_Delhi Daredevils', 'bat_team_Kings XI Punjab',
+match_df = match_df[['date', 'bat_team_Chennai Super Kings', 'bat_team_Delhi Capitals', 'bat_team_Kings XI Punjab',
               'bat_team_Kolkata Knight Riders', 'bat_team_Mumbai Indians', 'bat_team_Rajasthan Royals',
               'bat_team_Royal Challengers Bangalore', 'bat_team_Sunrisers Hyderabad',
-              'bowl_team_Chennai Super Kings', 'bowl_team_Delhi Daredevils', 'bowl_team_Kings XI Punjab',
+              'bowl_team_Chennai Super Kings', 'bowl_team_Delhi Capitals', 'bowl_team_Kings XI Punjab',
               'bowl_team_Kolkata Knight Riders', 'bowl_team_Mumbai Indians', 'bowl_team_Rajasthan Royals',
               'bowl_team_Royal Challengers Bangalore', 'bowl_team_Sunrisers Hyderabad',
               'overs', 'runs', 'wickets', 'runs_last_5', 'wickets_last_5', 'total']]
 
 # Splitting the data into train and test set
-X_train = encoded_df.drop(labels='total', axis=1)[encoded_df['date'].dt.year <= 2016]
-X_test = encoded_df.drop(labels='total', axis=1)[encoded_df['date'].dt.year >= 2017]
+X_train = match_df.drop(labels='total',axis=1)[match_df['date'].dt.year <= 2016]
+X_test = match_df.drop(labels='total',axis=1)[match_df['date'].dt.year >= 2017]
 
-y_train = encoded_df[encoded_df['date'].dt.year <= 2016]['total'].values
-y_test = encoded_df[encoded_df['date'].dt.year >= 2017]['total'].values
+y_train = match_df[match_df['date'].dt.year <= 2016]['total'].values
+y_test = match_df[match_df['date'].dt.year >= 2017]['total'].values
 
 # Removing the 'date' column
-X_train.drop(labels='date', axis=True, inplace=True)
-X_test.drop(labels='date', axis=True, inplace=True)
+X_train.drop(labels='date',axis=True, inplace=True)
+X_test.drop(labels='date',axis=True, inplace=True)
 
 # --- Model Building ---
 # Linear Regression Model
